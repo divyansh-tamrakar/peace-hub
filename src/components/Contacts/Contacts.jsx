870cee98abad9021a7b07bfe5aca61062b8af7e8
nyanishant <nayanishant.info@gmail.com>
2022-09-11 01:42:34 +0530

    Created contact

---
+++ src/components/Contacts/Contacts.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../Contacts/Contacts.css'
+
+const Contacts = () => {
+  return (
+    <div className='contact' id='contactId'>Contacts</div>
+  )
+}
+
+export default Contacts
