9d426ee6c5c505782d6d3c4ea36d2109a5350a68
nyanishant <nayanishant.info@gmail.com>
2022-09-11 01:43:04 +0530

    Added contact

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Contacts from './components/Contacts/Contacts';
 import Header from './components/Header/Header';
 import NavBar from './components/Navigation/NavBar';
 
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <NavBar />
       <Header />
+      <Contacts />
     </div>
   );
 }
