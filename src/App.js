58db1a216075e9436a7273b2a7ed8759ece79ceb
nyanishant <nayanishant.info@gmail.com>
2022-09-11 01:05:09 +0530

    New nav design and added header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Header from './components/Header/Header';
 import NavBar from './components/Navigation/NavBar';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../Header/Header.css'
+
+const Header = () => {
+  return (
+    <div className='header' id='header'>Header</div>
+  )
+}
+
+export default Header
