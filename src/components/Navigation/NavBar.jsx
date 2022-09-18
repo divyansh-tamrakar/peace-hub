6f63fdc771c8a023ee7138776c2055abe5e21aec
nyanishant <nayanishant.info@gmail.com>
2022-09-18 11:17:38 +0530

    Added site name and sign up button

--- src/components/Navigation/NavBar.jsx
+++ src/components/Navigation/NavBar.jsx
@@ -1,22 +1,31 @@
 import React from 'react'
 import '../Navigation/NavBar.css'
 import { Link } from 'react-scroll'
+import Button from '@mui/material/Button';
 
 const NavBar = () => {
   return (
-    <nav className='nav'>
-      <ul className='nav-links'>
-        <li>
-          <Link className="links" to="headerId" smooth={true} duration={1000}>Home</Link>
-        </li>
-        <li>
-          <Link className="links" to="aboutId" smooth={true} duration={1000}>About</Link>
-        </li>
-        <li>
-          <Link className="links" to="contactId" smooth={true} duration={1000}>Contact Us</Link>
-        </li>
-      </ul>
-    </nav>
+    <container className='parent'>
+      <header className='name'>
+        <h3>Nishant</h3>
+      </header>
+      <nav className='nav'>
+        <ul className='nav-links'>
+          <li>
+            <Link className="links" to="headerId" smooth={true} duration={1000}>Home</Link>
+          </li>
+          <li>
+            <Link className="links" to="aboutId" smooth={true} duration={1000}>About</Link>
+          </li>
+          <li>
+            <Link className="links" to="contactId" smooth={true} duration={1000}>Contact Us</Link>
+          </li>
+        </ul>
+      </nav>
+      <div className='signup'>
+        <Button variant="text" sx={{color:'#BC6FF1'}}>Sign Up</Button>
+      </div>
+    </container>
   )
 }
 
