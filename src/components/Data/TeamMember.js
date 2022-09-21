6b7220be95193db31f650b8a258b4206ba978f91
nyanishant <nayanishant.info@gmail.com>
2022-09-21 19:06:17 +0530

    added social links

--- src/components/Data/TeamMember.js
+++ src/components/Data/TeamMember.js
@@ -4,27 +4,27 @@ const TeamMembers = [
         'name': 'Nishant Nayan',
         'img': 'Gallery/01.jpg',
         'about': 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.',
-        'twitter': '',
-        'mail': '',
-        'linkedin': ''
+        'twitter': 'https://twitter.com/Nishant70607283',
+        'mail': 'https://twitter.com/Nishant70607283',
+        'linkedin': 'https://twitter.com/Nishant70607283'
     },
     {
         'id': 2,
         'name': 'Adarsh singh',
         'img': 'Gallery/01.jpg',
         'about': 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.',
-        'twitter': '',
-        'mail': '',
-        'linkedin': ''
+        'twitter': 'https://twitter.com/Nishant70607283',
+        'mail': 'https://twitter.com/Nishant70607283',
+        'linkedin': 'https://twitter.com/Nishant70607283'
     },
     {
         'id': 3,
         'name': 'Divyansh Tamrakar',
         'img': 'Gallery/01.jpg',
         'about': 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.',
-        'twitter': '',
-        'mail': '',
-        'linkedin': ''
+        'twitter': 'https://twitter.com/Nishant70607283',
+        'mail': 'https://twitter.com/Nishant70607283',
+        'linkedin': 'https://twitter.com/Nishant70607283'
     }
 ]
 
