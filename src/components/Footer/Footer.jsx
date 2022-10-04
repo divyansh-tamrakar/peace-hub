5f2369babca240af3f43bea1ab080d3c096ee261
nyanishant <nayanishant.info@gmail.com>
2022-10-04 22:44:20 +0530

    Fixed nav bar

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../Footer/Footer.css'
+
+const Footer = () => {
+  return (
+    <footer className='footer'>
+        <aside>
+          <h1>Logo</h1>
+        </aside>
+        <main></main>
+    </footer>
+  )
+}
+
+export default Footer
